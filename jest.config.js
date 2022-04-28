29526652fb8f654d46a95bffe1161f979945c85b
Luigi <66072698+lui-dias@users.noreply.github.com>
2022-04-27 23:05:58 -0300

    ignore: fazer testes funcionar

--- jest.config.js
+++ jest.config.js
@@ -168,19 +168,7 @@ module.exports = {
 
     // A map from regular expressions to paths to transformers
     transform: {
-        '^.+\\.(t|j)sx?$': [
-            '@swc/jest',
-            {
-                jsc: {
-                    parser: {
-                        syntax: 'ecmascript',
-                    },
-                },
-                module: {
-                    type: 'commonjs',
-                },
-            },
-        ],
+        "\\.[jt]sx?$": "babel-jest"
     },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
